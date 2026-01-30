326e8f230af33377fef672f54cbd73f57051a036
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2026-01-30 21:47:39 +0100

    Initialisation commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  const isDev = !app.isPackaged;
+  console.log("is Dev", isDev);
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:5173");
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../frontend/dist/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
