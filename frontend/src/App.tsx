a24f211ee2fea750ffe378c29b8774552b4a8661
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2026-02-01 16:00:03 +0100

    Save user to Firestore #1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,32 @@
 import { useState } from "react";
 import "./App.css";
 import signIn from "./features/auth/signIn";
-import { type UserCredential as FIRUser } from "firebase/auth";
+
 import signUserOut from "./features/auth/signOut";
+import createUser from "./features/user/createUser";
 
 function App() {
-  const [user, setUser] = useState<FIRUser | null>(null);
+  const [userID, setUserID] = useState<string | null>(null);
 
   async function handleSignIn() {
-    const userCred = await signIn();
-    setUser(userCred);
+    try {
+      const userCred = await signIn();
+      setUserID(userCred);
+      if (userCred !== null) await createUser(userCred);
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
   }
 
   async function handleSignOut() {
     await signUserOut();
-    setUser(null);
+    setUserID(null);
   }
 
   return (
     <>
       <button onClick={handleSignIn}>Sign in</button>
-      {user && <p>Signed in as {user.user.uid}</p>}
+      {userID && <p>Signed in as {userID}</p>}
 
       <button onClick={handleSignOut}>Sign out</button>
     </>
--- frontend/src/features/user/createUser.ts
+++ frontend/src/features/user/createUser.ts
@@ -1,1 +1,14 @@
-export default function createUser() {}
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/init";
+
+export default async function createUser(uid: string) {
+  // TODO: Change the first uid with a crypted uid after master password creation ( Issue #2 )
+  try {
+    await setDoc(doc(db, "users", uid), {
+      ownerUid: uid,
+      createdAt: new Date().toISOString(),
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+  }
+}
