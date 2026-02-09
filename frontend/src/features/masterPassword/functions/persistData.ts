08470a155e4685a287104d62eaca7eab8076dfc7
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2026-02-09 11:25:05 +0100

    Clean - Remove unused files #1

--- frontend/src/features/masterPassword/functions/persistData.ts
+++
@@ -1,4 +0,0 @@
-export default async function persistData(
-  publicID: string,
-  wrappedKey: string,
-): Promise<void> {}
--- frontend/src/features/passwords/screens/PasswordList.tsx
+++
@@ -1,9 +0,0 @@
-function PasswordList() {
-  return (
-    <div className="flex flex-col gap-2">
-      <p>No password for now</p>
-    </div>
-  );
-}
-
-export default PasswordList;
