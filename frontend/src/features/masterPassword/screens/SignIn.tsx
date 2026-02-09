c95b53b80f4d523739d3726a77e14723950e1e72
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2026-02-09 09:44:56 +0100

    Handle account creation #1

---
+++ frontend/src/features/masterPassword/screens/SignIn.tsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from "react";
+import generateRecoverySeed from "../../recoverySeed/generateRecoverySeed";
+import signIn from "../../auth/signIn";
+import createUser from "../../user/createUser";
+
+// import signIn from "./features/auth/signIn";
+
+// import signUserOut from "./features/auth/signOut";
+// import createUser from "./features/user/createUser";
+// import generateRecoverySeed from "./features/recoverySeed/generateRecoverySeed";
+
+interface Props {
+  masterPassword: string;
+  setMasterPassword: (password: string) => void;
+}
+
+enum PhraseStatus {
+  HIDDEN = "hidden",
+  SHOWN = "revealed",
+  COPIED = "copied",
+  COPIED_AND_REVEALED = "copied-and-revealed",
+}
+
+function SignIn({ masterPassword, setMasterPassword }: Props) {
+  const [showPhrase, setShowPhrase] = useState<boolean>(false);
+  const [phraseStatus, setPhraseStatus] = useState<PhraseStatus>(
+    PhraseStatus.HIDDEN,
+  );
+  const [copyButtonText, setCopyButtonText] =
+    useState<string>("Copier la phrase");
+  const [showInput, setShowInput] = useState<boolean>(false);
+  const [phrase, setPhrase] = useState<string>("");
+  // ?? Do we need this here, or globally, or not at all ??
+  const [userID, setUserID] = useState<string | null>(null);
+
+  function handleMnemonicCopy() {
+    navigator.clipboard.writeText(phrase);
+
+    switch (phraseStatus) {
+      case PhraseStatus.SHOWN:
+        setPhraseStatus(PhraseStatus.COPIED);
+        setShowPhrase(false);
+        setShowInput(true);
+        setCopyButtonText("Revoir la phrase");
+        break;
+      case PhraseStatus.COPIED:
+        setShowPhrase(true);
+        setShowInput(false);
+        setPhraseStatus(PhraseStatus.COPIED_AND_REVEALED);
+        setCopyButtonText("Copier la phrase");
+        break;
+      case PhraseStatus.COPIED_AND_REVEALED:
+        setPhraseStatus(PhraseStatus.COPIED);
+        setShowPhrase(false);
+        setShowInput(true);
+        setCopyButtonText("Revoir la phrase");
+        break;
+      default:
+        setPhraseStatus(PhraseStatus.COPIED);
+        break;
+    }
+  }
+
+  async function handleSignIn() {
+    try {
+      const userCred = await signIn();
+      setUserID(userCred);
+
+      const mnemonic = generateRecoverySeed();
+      setPhrase(mnemonic);
+
+      if (userCred !== null)
+        await createUser(userCred, mnemonic, masterPassword);
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      const mnemonic = generateRecoverySeed();
+      setPhrase(mnemonic);
+      setPhraseStatus(PhraseStatus.SHOWN);
+      setShowPhrase(true);
+    }, 1500);
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h1>Bonjour, et bienvenue sur Locky!</h1>
+
+      {showPhrase && (
+        <div className="flex flex-col items-center gap-2">
+          <h2 className="text-start">
+            Pour commencer, veuillez noter précieusement la phrase suivante.
+          </h2>
+          <p>
+            Elle vous permettra de vous authentifier dans le cas où vous
+            perdriez votre mot de passe.
+          </p>
+          <p>{phrase}</p>
+        </div>
+      )}
+      {phraseStatus !== PhraseStatus.HIDDEN && (
+        <button onClick={handleMnemonicCopy}>{copyButtonText}</button>
+      )}
+      {showInput && (
+        <div className="flex flex-col justify-center items-center">
+          <p>Entrez maintenant votre mot de passe :</p>
+          <input
+            value={masterPassword}
+            onChange={(e) => setMasterPassword(e.target.value)}
+            className="border border-gray-300 rounded-md p-2 w-full"
+            placeholder="Master Password"
+            type="password"
+          />
+        </div>
+      )}
+
+      {masterPassword !== "" && (
+        <button onClick={handleSignIn}>Entrer dans l'application</button>
+      )}
+
+      {userID && (
+        <div className="flex flex-col items-center gap-2">
+          <h2 className="text-start">
+            Vous êtes connecté en tant que {userID}.
+          </h2>
+          {/*<button onClick={handleSignOut}>Se déconnecter</button>*/}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SignIn;
---
+++ frontend/src/features/passwords/screens/PasswordList.tsx
@@ -0,0 +1,9 @@
+function PasswordList() {
+  return (
+    <div className="flex flex-col gap-2">
+      <p>No password for now</p>
+    </div>
+  );
+}
+
+export default PasswordList;
