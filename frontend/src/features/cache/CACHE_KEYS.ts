30392dc39fe9b3c85058bdc865d06a38677eba08
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2026-02-16 17:02:21 +0100

    Add core methods to handle Touch ID #10

--- frontend/src/features/cache/CACHE_KEYS.ts
+++ frontend/src/features/cache/CACHE_KEYS.ts
@@ -1,6 +1,7 @@
 enum CACHE_KEYS {
   PUBLIC_ID = "publicID",
   PIN_WRAP = "pinWrap",
+  BIOMETRICS_WRAP = "biometricsWrap",
 }
 
 export default CACHE_KEYS;
--- preload.js
+++ preload.js
@@ -3,5 +3,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electron", {
   // Renderer to main
   // Main to renderer ( Two-way)
+  promptTouchID: (reason) => ipcRenderer.invoke("touch-id:prompt", reason),
+  encrypt: (data) => ipcRenderer.invoke("touch-id:encrypt", data),
+  decrypt: (blob) => ipcRenderer.invoke("touch-id:decrypt", blob),
   // Event Listeners (Main to Renderer)
 });
