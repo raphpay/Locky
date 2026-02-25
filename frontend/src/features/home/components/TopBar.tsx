f4f63f4f68214aef3839f0f670bad20ef3524138
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2026-02-25 14:41:49 +0100

    Update components #13

--- frontend/src/features/home/components/TopBar.tsx
+++ frontend/src/features/home/components/TopBar.tsx
@@ -1,22 +1,38 @@
-import { ArrowUpDown, Plus, User } from "lucide-react";
+import { Plus, User } from "lucide-react";
 import { Button } from "../../../ui/components/radix/Button";
 import { SearchInput } from "../../../ui/components/custom/SearchInput";
+import SortingDropdown from "./SortingDropdown";
+import type SORTING_SELECTION from "../sort/sortingSelection";
 
 interface Props {
   searchQuery: string;
   setSearchQuery: (value: string) => void;
+  sortingSelection: SORTING_SELECTION;
+  onSortSelectionChange: (selection: SORTING_SELECTION) => void;
+  isSortingAscending: boolean;
+  onSortIsAscendingChange: (newValue: boolean) => void;
 }
 
-function TopBar({ searchQuery, setSearchQuery }: Props) {
+function TopBar({
+  searchQuery,
+  setSearchQuery,
+  sortingSelection,
+  onSortSelectionChange,
+  isSortingAscending,
+  onSortIsAscendingChange,
+}: Props) {
   return (
     <div className="flex w-full justify-between absolute top-8 left-8">
       <div className="flex gap-2">
         <Button variant={"secondary"}>
           <User />
         </Button>
-        <Button variant={"secondary"}>
-          <ArrowUpDown />
-        </Button>
+        <SortingDropdown
+          sortingSelection={sortingSelection}
+          onSortSelectionChange={onSortSelectionChange}
+          isSortingAscending={isSortingAscending}
+          onSortIsAscendingChange={onSortIsAscendingChange}
+        />
         <Button variant={"secondary"}>
           <Plus />
         </Button>
