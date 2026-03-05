135000768dbff8dd57703f498d5d460ef2d1d815
Raphaël Payet <raphaelpayet@MacBook-Pro-de-Raphael.local>
2026-03-05 18:17:31 +0100

    Change loading spinner colors #13

--- frontend/src/features/auth/components/LoadingSpinner.tsx
+++ frontend/src/features/auth/components/LoadingSpinner.tsx
@@ -8,11 +8,11 @@ const LoadingSpinner = ({ isLoading }: Props) => {
   return (
     <>
       {isLoading && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/20 backdrop-blur-[1px]">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-background/20 backdrop-blur-[1px]">
           {/* Box blanche ou transparente pour le spinner */}
-          <div className="bg-white p-6 rounded-xl shadow-xl flex flex-col items-center gap-3">
-            <Spinner className="size-10 text-black" />
-            <p className="text-sm font-medium text-gray-700">
+          <div className="bg-background text-primary-text p-6 rounded-xl shadow-xl flex flex-col items-center gap-3">
+            <Spinner className="size-10 text-primary" />
+            <p className="text-sm font-medium text-secondary-text">
               Chargement en cours...
             </p>
           </div>
